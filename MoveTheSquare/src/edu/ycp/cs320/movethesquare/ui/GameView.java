9e6ccad8b3d9b8691f802d39136a7a9306c731f3
Esherymack <esherymack@gmail.com>
2019-03-13 11:30:20 -0400

    New colors (Weird_Purple and Weird_Green) and now it's a rectangle.
    Rectangles are fun.

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -17,7 +17,8 @@ import edu.ycp.cs320.movethesquare.model.Game;
 import edu.ycp.cs320.movethesquare.model.Square;
 
 public class GameView extends JPanel {
-	private static final Color MIDNIGHT_BLUE = new Color(25, 25, 112);
+	private static final Color WEIRD_PURPLE = new Color(125, 66, 244);
+	private static final Color WEIRD_GREEN = new Color(163, 244, 65);
 	
 	private Game model;
 	private GameController controller;
@@ -26,7 +27,7 @@ public class GameView extends JPanel {
 	public GameView(Game model) {
 		this.model = model;
 		setPreferredSize(new Dimension((int) model.getWidth(), (int)model.getHeight()));
-		setBackground(MIDNIGHT_BLUE);
+		setBackground(WEIRD_PURPLE);
 
 		// djh2-KEC119-21: changed from 30 to 45
 		// djh2-YCPlaptop: change from 45 to 100
@@ -66,7 +67,7 @@ public class GameView extends JPanel {
 		
 		// djh2-KEC110-21: changed from GREEN to RED
 		// djh2-YCPlaptop: change from RED to YELLOW
-		g.setColor(Color.YELLOW);
+		g.setColor(WEIRD_GREEN);
 
 		Square square = model.getSquare();
 		
@@ -84,8 +85,8 @@ public class GameView extends JPanel {
 				Square square = new Square();
 				square.setX(300.0);
 				square.setY(220.0);
-				square.setWidth(40.0);
-				square.setHeight(40.0);
+				square.setWidth(60.0);
+				square.setHeight(20.0);
 				model.setSquare(square);
 				
 				GameController controller = new GameController();
